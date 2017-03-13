8adb43b597f185ab6d0ea8d38f5dc16f1c44b485
Chris Manders <cmanders159@gmail.com>
2017-03-13 20:22:10 +0000

    Login verification implemented

--- Resources/php/apirequester.php
+++ Resources/php/apirequester.php
@@ -2,19 +2,23 @@
     error_reporting(E_ALL); // Errors on
     ini_set('display_errors', 1);
         
-    header('Content-type: application/xml'); // Set data type
+    header('Content-type: text/xml'); // Set data type
 
-    $username = 'cmanders.tech';
-    $password = 'a47325f0e9d8a877576edd687fa1a7b4';
-    $url = 'https://cloudops-jenkins-ust1.hostedtest.act.com:8443/me/api/xml'; // Set url with group id and field selection from URL string
+    $url = $_GET["url"];
+    $token = $_GET["token"];
     
     $context = stream_context_create(array( // Authorise basic
         'http' => array(
-            'header'  => "Authorization: Basic " . base64_encode("$username:$password")
+            'header'  => "Authorization: " . $token
         )
     ));
 
     $data = file_get_contents($url, false, $context); // Get the contents of the URL
-    
-    echo $data; // Show the returned data
+
+    if (strpos($data, "enabledColorBlindSupport")){
+        echo $data; // Show the returned data
+    }
+    else {
+        http_response_code(401);
+    }
 ?>
