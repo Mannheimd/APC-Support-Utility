7f34688a539776033be44df64882b8289aadffd4
Chris Manders <cmanders159@gmail.com>
2018-07-19 15:46:45 +0100

    Implement parsing of lookup output

--- resources/javascript/jenkinsLookup.js
+++ resources/javascript/jenkinsLookup.js
@@ -21,12 +21,29 @@ function jenkinsLookup(rawLookupOutput) {
     data.suspendStatus = findString(rawLookupOutput, "[SuspendStatus=", "]").trim();
     data.archiveStatus = findString(rawLookupOutput, "[ArchiveStatus=", "]").trim();
     data.deleteStatus = findString(rawLookupOutput, "[DeleteStatus=", "]").trim();
+
     data.siteInfoFound = findString(rawLookupOutput, "[SiteInfoFound=", "]").trim();
     data.siteInfoArray = findString(rawLookupOutput, "[SITEINFOSTART]", "[SITEINFOEND]").split("[SiteInfo=");
     data.siteInfo = [];
     for (var i = 0; i < data.siteInfoArray.length; i++) {
-        if (data.siteInfoArray[i] == "\r\n") {continue};
+        if (data.siteInfoArray[i] == "\r\n" || data.siteInfoArray[i] == undefined) {continue};
         data.siteInfo.push(parseSiteInfo(data.siteInfoArray[i]));
+    }
+
+    data.databaseInfoFound = findString(rawLookupOutput, "[DatabaseInfoFound=", "]").trim();
+    data.databaseInfoArray = findString(rawLookupOutput, "[DATABASEINFOSTART]", "[DATABASEINFOEND]").split("[Database=");
+    data.databaseInfo = [];
+    for (var i = 0; i < data.siteInfoArray.length; i++) {
+        if (data.databaseInfoArray[i] == "\r\n" || data.databaseInfoArray[i] == undefined) {continue};
+        data.databaseInfo.push(parseDatabaseInfo(data.databaseInfoArray[i]));
+    }
+
+    data.activityInfoFound = findString(rawLookupOutput, "[ProvisioningInfoFound=", "]").trim();
+    data.activityInfoArray = findString(rawLookupOutput, "[PROVISIONINGINFOSTART]", "[PROVISIONINGINFOEND]").split("[Activity=");
+    data.activityInfo = [];
+    for (var i = 0; i < data.activityInfoArray.length; i++) {
+        if (data.activityInfoArray[i] == "\r\n" || data.activityInfoArray[i] == undefined) {continue};
+        data.activityInfo.push(parseActivityInfo(data.activityInfoArray[i]));
     }
 
     function findString(text, startString, endString) {
@@ -43,6 +60,22 @@ function jenkinsLookup(rawLookupOutput) {
         data.iisServer = findString(siteInfoText, "{IISServer=", "}").trim();
         data.url = findString(siteInfoText, "{URL=", "}").trim();
         data.uploadUrl = findString(siteInfoText, "{UploadURL=", "}").trim();
+        return data;
+    }
+
+    function parseDatabaseInfo(databaseInfoText) {
+        var data = {};
+        data.name = findString(databaseInfoText, "{Name=", "}").trim();
+        data.server = findString(databaseInfoText, "{Server=", "}").trim();
+        return data;
+    }
+
+    function parseActivityInfo(activityInfoText) {
+        var data = {};
+        data.date = findString(activityInfoText, "{Date=", "}").trim();
+        data.type = findString(activityInfoText, "{Type=", "}").trim();
+        data.status = findString(activityInfoText, "{Status=", "}").trim();
+        data.detail = findString(activityInfoText, "{Detail=", "}").trim();
         return data;
     }
 };
