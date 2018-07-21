9ce9bd078159754d8e25565bdd8fe81105b68719
Chris Manders <cmanders159@gmail.com>
2018-07-21 17:02:34 +0100

    Build database details layout and tabs

--- resources/javascript/actDatabase.js
+++ resources/javascript/actDatabase.js
@@ -21,6 +21,9 @@ actDatabase.prototype.addDatabaseListItem = function(database, targetList, looku
 actDatabase.prototype.switchToDatabase = function(database) {
     database.detailsHtml = actDatabase.prototype.processTemplate($("#glcLookupActDatabaseDetailsTpl").html(), database);
     $("#glcLookupDatabaseDetails").html(database.detailsHtml);
+
+    actDatabase.prototype.addButtonBindings(database);
+
     var listItem = $("#glcLookupActDatabaseListItem" + database.number);
     listItem.prop("checked", true);
 }
@@ -31,4 +34,18 @@ actDatabase.prototype.processTemplate = function(html, database) {
     htmlAltered = replaceAllInstances(htmlAltered, "{{databaseName}}", database.name);
     htmlAltered = replaceAllInstances(htmlAltered, "{{databaseServer}}", database.server);
     return htmlAltered;
+}
+
+actDatabase.prototype.addButtonBindings = function(database) {
+    $("#glcDatabaseDetailsScreenSelection").on("click", "ul > li", function(e) {
+        database.screenSelectionPageId = $(e.target).attr("data-pageId");
+        actDatabase.prototype.setScreenSelectionPage(database);
+    })
+}
+
+actDatabase.prototype.setScreenSelectionPage = function(database) {
+    if (database.screenSelectionPageId) {
+        changePage("glcDatabaseDetailsScreenSelection", database.screenSelectionPageId);
+        changeTab("glcDatabaseDetailsScreenSelection", database.screenSelectionPageId);
+    }
 }
